2fe1e8352c977b3c17bbad51fffa76768e120963
Suelen Bueno <suelen.fbueno@gmail.com>
2026-03-11 22:50:36 -0300

    feat: adicionar componente Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { Header } from './components/Header';
-import { Hero } from './components/Hero';
+import React from "react";
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 function App() {
-
   return (
     <>
       <Header />
       <main className="py-10">
-       <Hero />
+        <Hero />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
